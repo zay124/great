c01d7c7c977ea2bf72dc3fa53d6201c0246d4cfc
Zay124 <agroaissa@gmail.com>
2018-03-08 19:37:00 +0100

    correction de index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 // au chargement de doc, branchement des evenements
 window.onload= function(){
        //recupère un tableau des images
-        var  pics = document.querySelectorAll('article.articleMetier>img')
+        var  pics = document.querySelectorAll('article.articleMetier>img');
 // passe en revue chaque img
- for (var i=0;i<pics.length;i++){
+ for (var i = 0; i < pics.length;i++){
      // branche 3 ecouteurs d'evenement à l'image
      pics[i].addEventListener('mouseover',creerPrev,true);
      pics[i].addEventListener('mousemove',deplacerPrev,true);
-     pics[i].addEventListener('mouseout',supprPrev,true);
- };
+     pics[i].addEventListener('mouseout', supprPrev, true);
+ }
 };
 
 
@@ -16,34 +16,34 @@ window.onload= function(){
 function creerPrev(evt){
      // creer un la volet un div 
      var prev = document.createElement('div')
-       prev.id='preview';
-       prev.style.position='absolue';
-       prev.style.left=(evt.pageX+10)+'px';
-       prev.style.top=(evt.pageY+10)+'px';
-       prev.style.width='300px';
-       prev.style.height='100px';
-       prev.style.zIndex='999';
-       prev.style.backgroundColor='yellow';
-       prev.style.border='black solid 1px';
+       prev.id = 'preview';
+       prev.style.position = 'absolute';
+       prev.style.left=(evt.pageX + 10 ) + 'px';
+       prev.style.top=(evt.pageY + 10) + 'px';
+       prev.style.width = '300px';
+       prev.style.height = '100px';
+       prev.style.zIndex = '999';
+       prev.style.backgroundColor = 'red !important';
+       prev.style.border = 'black solid 1px';
        prev.textContent =evt.target.nextSibling.href;
        // Rattache au doc
        document.body.appendChild(prev);
 
-};
+}
 
 // fonction qui gère le diplacement de la souris
 
 function deplacerPrev(evt){
 var prev = document.getElementById('preview');
-prev.style.left=(evt.pageX+ 10)+'px';
-prev.style.top=(evt.pageY+ 10)+'px';
+prev.style.left = (evt.pageX+ 10)+'px';
+prev.style.top = (evt.pageY+ 10)+'px';
 
-};
+}
 
 // fonction qui gère la sortie de la souris
 
 function supprPrev(){
-    var evt=document.getElementById('preview');
+    var prev =document.getElementById('preview');
     document.body.removeChild(prev);
 
-};
+}
